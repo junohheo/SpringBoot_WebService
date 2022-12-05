483973296d9773290e8e6f3bd90137b90a8c2c7f
junohheo <bbq0400@naver.com>
2022-12-05 17:58:43 +0900

    firstCommit

---
+++ src/main/java/com/example/springboot_webservice/SpringBootWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebServiceApplication.class, args);
+    }
+
+}
