507e58afe4efc7a9272a684745cb8da912f90cc4
junohheo <bbq0400@naver.com>
2022-12-13 16:30:32 +0900

    Feat: 게시글 전체 조회 적용

--- src/main/java/com/example/springboot_webservice/service/posts/PostsService.java
+++ src/main/java/com/example/springboot_webservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springboot_webservice.service.posts;
 
 import com.example.springboot_webservice.domain.post.Posts;
 import com.example.springboot_webservice.domain.post.PostsRepository;
+import com.example.springboot_webservice.web.dto.PostsListResponseDto;
 import com.example.springboot_webservice.web.dto.PostsResponseDto;
 import com.example.springboot_webservice.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
                 + id));
 
         return new PostsResponseDto(entity);
+    }
+
+   @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
