cabfe31a59b776506d1cceb6665d4855b15f3851
junohheo <bbq0400@naver.com>
2022-12-24 22:31:47 +0900

    Feat:  CustomOAuth2UserService

---
+++ src/main/java/com/example/springboot_webservice/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,49 @@
+package com.example.springboot_webservice.config.auth.dto;
+
+import com.example.springboot_webservice.domain.user.Role;
+import com.example.springboot_webservice.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes,String nameAttributeKey, String name, String email, String picture) {
+    this.attributes = attributes;
+    this.nameAttributeKey = nameAttributeKey;
+    this.email = email;
+    this.picture = picture;
+    this.name = name;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
