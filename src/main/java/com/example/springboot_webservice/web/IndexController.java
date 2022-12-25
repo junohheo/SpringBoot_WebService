2716120eb2974b8e90c51715622455a372cc88ec
junohheo <bbq0400@naver.com>
2022-12-25 18:10:01 +0900

    Feat: 세션 저장소 데이터베이스로 교체

--- src/main/java/com/example/springboot_webservice/web/IndexController.java
+++ src/main/java/com/example/springboot_webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springboot_webservice.web;
 
+import com.example.springboot_webservice.config.auth.LoginUser;
 import com.example.springboot_webservice.config.auth.dto.SessionUser;
 import com.example.springboot_webservice.service.posts.PostsService;
 import com.example.springboot_webservice.web.dto.PostsResponseDto;
@@ -17,9 +18,8 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
 
